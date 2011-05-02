ee433f741d2ed7f83541a50c5e410061242deb26
Aurelien Giraud <augiraud@gmail.com>
2011-05-02 12:12:27 +0200

    fixed login template and handler

--- charlie/test_manager/views.py
+++ charlie/test_manager/views.py
@@ -1,22 +1,23 @@
-from django.core import serializers
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from test_manager.models import *
-from django.core.context_processors import csrf
 from test_manager.classes import *
+from django.core.context_processors import csrf
+import pickle
 
 def login(request):
-    c = {}
-    c.update(csrf(request))
-    return render_to_response('tests/login.html', c)
-
-def login_attempt(request):
-    c = {}
-    c.update(csrf(request))
-    visa = request.POST['visa']
-    password = request.POST['pass']
-    connection = JiraConnection(visa, password)
-    return serializers.serialize('json', connection)
+    if request.method == "GET":
+        c = {}
+        c.update(csrf(request))
+        return render_to_response('tests/login.html', c)
+    elif request.method == "POST":
+        jc = Jiraconnection(request.POST['visa'], request.POST['passwd'])
+        if jc.success:
+            return HttpResponse("connected")
+        else:
+            return HttpResponse("failed to connect")
+    else:
+        return HttpResponse("bad http request")
 
 def create_tc(request):
     return render_to_response('tests/create_tc.html')
