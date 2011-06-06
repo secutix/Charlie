1b3f6a1a4b7c94209140e47705103ea9f2bd0986
AGRaud <augiraud@gmail.com>
2011-06-06 04:38:53 -0700

    Edited charlie/setup.py via GitHub

--- charlie/setup.py
+++ charlie/setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 setup(
     name = 'charlie_test_manager',
     version = '0.1',
-    packages = find_packages(),
+    packages = ['charlie'],
     author = 'AGRaud',
     author_email = 'augiraud@gmail.com',
     description = 'Software Testing Manager',
     url = 'https://github.com/nire/Charlie',
     include_package_data = True,
-    depends = 'django suds memcached python-memcached django-nose ez_setup virtualenv simplejson'
+    requires = 'django suds memcached python-memcached django-nose ez_setup virtualenv simplejson'
 )
