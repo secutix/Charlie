618ac3ed97698818cbc944c1d659aa7995dce6d2
Aurelien Giraud <augiraud@gmail.com>
2011-06-06 10:44:13 +0200

    new setup.py (for testing only)

---
+++ charlie/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'charlie_test_manager',
+    version = '0.1',
+    packages = find_packages(),
+    author = 'AGRaud',
+    author_email = 'augiraud@gmail.com',
+    description = 'Software Testing Manager',
+    url = 'https://github.com/nire/Charlie',
+    include_package_data = True,
+    depends = 'django suds memcached python-memcached django-nose ez_setup virtualenv simplejson'
+)
