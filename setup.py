fc843ea984ee00597d63ae5d32e5c66281333cbb
Aurelien Giraud <augiraud@gmail.com>
2011-06-06 12:04:30 +0200

    moved setup to parent directory

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = 'charlie_test_manager',
     version = '0.1',
-    packages = find_packages(),
+    packages = ['charlie'],
     author = 'AGRaud',
     author_email = 'augiraud@gmail.com',
     description = 'Software Testing Manager',
